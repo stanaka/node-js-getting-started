5bb8402e67436d78a12085f18c98714786d90cae
Shinji Tanaka <shinji.tanaka@gmail.com>
2015-10-19 12:53:29 +0900

    update

--- start.sh
+++ start.sh
@@ -3,10 +3,11 @@ cat >/app/mackerel-agent/mackerel-agent.conf <<'EOF';
 pidfile = "/app/mackerel-agent/mackerel-agent.pid"
 root = "/app/mackerel-agent"
 verbose = true
-apikey = "<APIKEY>"
+apikey = "9pPrfeGDpAy75pUrC3NYii3YSH8vZ2CGu5L7sWrBuUtV"
 EOF
 SCRIPT
 echo >> /app/mackerel-agent/mackerel-agent.conf
 cat /app/mackerel-agent/mackerel-agent.conf
 /app/mackerel-agent/mackerel-agent -conf /app/mackerel-agent/mackerel-agent.conf -v &
 node index.js
+/app/mackerel-agent/mackerel-agent retire -conf /app/mackerel-agent/mackerel-agent.conf -force
