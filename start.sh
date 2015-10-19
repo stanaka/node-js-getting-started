9efea3b0785c154f721e66361c448a26c87fbea4
Shinji Tanaka <shinji.tanaka@gmail.com>
2015-10-19 12:58:25 +0900

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+cd /app/ && curl -O http://file.mackerel.io/agent/tgz/mackerel-agent-latest.tar.gz && tar xvfz mackerel-agent-latest.tar.gz
 sh << SCRIPT
 cat >/app/mackerel-agent/mackerel-agent.conf <<'EOF';
 pidfile = "/app/mackerel-agent/mackerel-agent.pid"
